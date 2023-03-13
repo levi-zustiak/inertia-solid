33b99ee19105564211b8abbbe8f88fe25f1557f3
lzustiak <levizustiak@gmail.com>
2023-03-13 00:11:56 -0500

    [feat] bundle config for rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,39 @@
-import pkg from './package.json' assert { type: "json" };
-import typescript from '@rollup/plugin-typescript';
-import resolve from '@rollup/plugin-node-resolve';
+import pkg from "./package.json" assert { type: "json" };
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import { babel } from "@rollup/plugin-babel";
+import terser from "@rollup/plugin-terser";
+
+const extensions = [".js", ".jsx", ".ts", ".tsx"];
 
 export default {
-  input: 'src/index.ts',
+  input: "src/index.ts",
+  external: ["solid-js", "solid-js/web"],
   output: [
     {
       file: pkg.main,
-      format: 'cjs',
+      format: "cjs",
     },
     {
       file: pkg.module,
-      format: 'esm',
-    }
+      format: "esm",
+    },
   ],
   plugins: [
-    typescript({
-      tsconfig: "./tsconfig.json"
+    resolve({ extensions }),
+    typescript({ tsconfig: "./tsconfig.json" }),
+    commonjs(),
+    json(),
+    babel({
+      extensions,
+      babelHelpers: "bundled",
+      presets: [
+        ["babel-preset-solid", {}],
+        "@babel/preset-typescript",
+      ],
     }),
-    resolve({
-      extensions: ['.js', '.ts', '.tsx']
-    })
-  ]
-}
+    terser(),
+  ],
+};
