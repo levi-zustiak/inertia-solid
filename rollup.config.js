062f435b9a210ae1502e0a861b5fd6c8cfeff96e
Levi Zustiak <91484241+lzustiak@users.noreply.github.com>
2023-03-21 23:49:18 -0500

    [feat] update package dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,35 @@
 import pkg from "./package.json" assert { type: "json" };
 import typescript from "@rollup/plugin-typescript";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
 import { babel } from "@rollup/plugin-babel";
-import terser from "@rollup/plugin-terser";
+import dts from "rollup-plugin-dts";
+import del from "rollup-plugin-delete";
 
 const extensions = [".js", ".jsx", ".ts", ".tsx"];
 
-export default {
-  input: "src/index.ts",
-  external: ["solid-js", "solid-js/web"],
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-    },
-    {
+export default [
+  {
+    input: "src/index.ts",
+    external: ["solid-js", "solid-js/web", "solid-js/store", "@inertiajs/core"],
+    output: {
       file: pkg.module,
       format: "esm",
     },
-  ],
-  plugins: [
-    resolve({ extensions }),
-    typescript({ tsconfig: "./tsconfig.json" }),
-    commonjs(),
-    json(),
-    babel({
-      extensions,
-      babelHelpers: "bundled",
-      presets: [
-        ["babel-preset-solid", {}],
-        "@babel/preset-typescript",
-      ],
-    }),
-    terser(),
-  ],
-};
+    plugins: [
+      json(),
+      babel({
+        extensions,
+        babelHelpers: "bundled",
+        presets: ["babel-preset-solid", {}],
+      }),
+      nodeResolve({ extensions }),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "dist/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts(), del({ hook: "buildEnd", targets: "./dist/types" })],
+  },
+];
